f54c083588506f97f21074929782b3d370fe71b1
Chad Ingram <chad@emtrain.com>
2015-02-01 16:08:52 -0800

    Version string tweaks.

--- lib/SAGN/version.rb
+++ lib/SAGN/version.rb
@@ -1,8 +1,10 @@
+# Conforms to Semantic Versioning 2.0
+# See http://semver.org/ for details.
 module SAGN
-  MAJOR = '0'
-  MINOR = '1'
-  PATCH = '0'
-  BUILD = 'pre'
-  REVISION = `git rev-parse HEAD`.gsub("\n",'')[0..6]
-  VERSION  = "#{MAJOR}.#{MINOR}.#{PATCH}-#{BUILD}.#{REVISION}"
+  MAJOR    = '0'
+  MINOR    = '1'
+  PATCH    = '0'
+  PRE      = 'beta1'
+  SHA      = `git rev-parse HEAD`.gsub("\n",'')
+  VERSION  = "#{MAJOR}.#{MINOR}.#{PATCH}-#{PRE}.build-#{SHA[0..6]}"
 end
