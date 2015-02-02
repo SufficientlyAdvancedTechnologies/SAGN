0b6b86138d66062d90809020202f6e3a4f8d1fae
Chad Ingram <chad@emtrain.com>
2015-02-01 16:04:12 -0800

    Fix up version #s

--- lib/SAGN/version.rb
+++ lib/SAGN/version.rb
@@ -1,3 +1,8 @@
 module SAGN
-  VERSION = "0.1.0.pre"
+  MAJOR = '0'
+  MINOR = '1'
+  PATCH = '0'
+  BUILD = 'pre'
+  REVISION = `git rev-parse HEAD`.gsub("\n",'')[0..6]
+  VERSION  = "#{MAJOR}.#{MINOR}.#{PATCH}-#{BUILD}.#{REVISION}"
 end
